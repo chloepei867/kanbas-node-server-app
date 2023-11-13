169506e510ac478e1de9d386c6dab62d3c45c951
Zhiyan Pei <chloep@zhiyp.lan>
2023-11-13 11:17:45 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./Courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import "dotenv/config";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Hello(app);
+Lab5(app);
+// app.listen(process.env.PORT || 4000);
+app.listen(4000);
