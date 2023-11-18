032f5447e2e4e98cb4f8fd4a8bd695fa9a5d1e20
Zhiyan <pei.zhiy@northeastern.edu>
2023-11-18 13:34:50 -0800

    submit 11.8 1st

--- Modules/routes.js
+++ Modules/routes.js
@@ -34,9 +34,9 @@ function ModuleRoutes(app) {
     res.send(modules);
   });
 
-  // app.get("/api/modules", (req, res) => {
-  //   const modules = Database.modules;
-  //   res.send(modules);
-  // });
+  app.get("/api/modules", (req, res) => {
+    const modules = db.modules;
+    res.send(modules);
+  });
 }
 export default ModuleRoutes;
