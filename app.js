786e525a6dc299b294bef2c357bc6450f9a20e23
Zhiyan <pei.zhiy@northeastern.edu>
2023-11-30 21:57:56 -0800

    submit 11.30 3rd

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-import express from "express";
-import Hello from "./hello.js";
-import Lab5 from "./Lab5.js";
-import cors from "cors";
-import mongoose from "mongoose";
-import UserRoutes from "./users/routes.js";
-import session from "express-session";
-import "dotenv/config";
+import express from 'express'
+import Hello from './hello.js'
+import Lab5 from './Lab5.js'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import UserRoutes from './users/routes.js'
+import session from 'express-session'
+import 'dotenv/config'
 
-import CourseRoutes from "./Courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
-import "dotenv/config";
+import CourseRoutes from './Courses/routes.js'
+import ModuleRoutes from './Modules/routes.js'
+import 'dotenv/config'
 
 const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+  process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 // process.env.DB_CONNECTION_STRING || "mongodb://0.0.0.0:27017/kanbas";
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING)
 // mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
-const app = express();
+const app = express()
 app.use(
   cors({
     credentials: true,
     // origin: process.env.FRONTEND_URL,
-    origin: "https://a6--super-palmier-61d023.netlify.app",
-    "Access-Control-Allow-Origin": "*",
+    origin: 'https://a6--super-palmier-61d023.netlify.app',
+    'Access-Control-Allow-Origin': '*',
   })
-);
+)
 
 const sessionOptions = {
-  secret: "any string",
+  secret: 'any string',
   resave: false,
   saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-  };
 }
-app.use(session(sessionOptions));
+if (process.env.NODE_ENV !== 'development') {
+  sessionOptions.proxy = true
+  sessionOptions.cookie = {
+    sameSite: 'none',
+    secure: true,
+  }
+}
+app.use(session(sessionOptions))
 
-app.use(express.json());
-UserRoutes(app);
-ModuleRoutes(app);
-CourseRoutes(app);
-Hello(app);
-Lab5(app);
-app.listen(process.env.PORT || 4000);
+app.use(express.json())
+UserRoutes(app)
+ModuleRoutes(app)
+CourseRoutes(app)
+Hello(app)
+Lab5(app)
+app.listen(process.env.PORT || 4000)
 // app.listen(4000);
